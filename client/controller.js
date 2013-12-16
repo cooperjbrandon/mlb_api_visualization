09ceececcc41b71e80bc3073593a79eedc0a5fbf
Brandon Cooper <cooperjbrandon@gmail.com>
2013-12-16 15:30:59 -0800

    added button on main.html to get stats. node server retrieves stats from gameday API

--- client/controller.js
+++ client/controller.js
@@ -1,6 +1,15 @@
 angular.module('mlbApp')
-.controller('MainController', function($scope) {
+.controller('MainController', function($scope, $http) {
 	$scope.hello = "hi";
+	$scope.stats = function () {
+		$http({
+    	method: 'GET',
+    	url: '/stats'
+  	})
+  	.then(function(data) {
+  		$scope.stats = data.data;
+  	});
+	};
 })
 .controller('MLBController', function($scope) {
   
--- server/router.js
+++ server/router.js
@@ -1,4 +1,6 @@
-var fs = require('fs');
+var
+  fs    = require('fs'),
+  http  = require('http');
 
 var headers = {};
 
@@ -25,12 +27,32 @@ var jsFiles = function(request, response, pathname) {
   readFile(response, pathname);
 };
 
+var stats = function() {
+  var data = '';
+  var options = {
+    host: 'gd2.mlb.com',
+    path: '/components/game/mlb/year_2008/month_04/day_07/master_scoreboard.xml'
+  };
+  http.get(options, function(result) {
+    result.on("data", function(chunk) {
+      data += chunk;
+    });
+    result.on('end', function() {
+      console.log(data);
+    });
+  }).on('error', function(e) {
+      console.log("Error: " + options.host + "\n" + e.message); 
+      console.log( e.stack );
+  });
+};
+
 var router = {
   '/': htmlFiles,
   '/templates/main.html': htmlFiles,
   '/templates/MLB.html': htmlFiles,
   '/client/app.js': jsFiles,
-  '/client/controller.js': jsFiles
+  '/client/controller.js': jsFiles,
+  '/stats': stats
 };
 
 exports.router = router;
